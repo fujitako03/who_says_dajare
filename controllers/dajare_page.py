e5b1f8ce1231764cef78be1d51c4d15f7b6fbe2d
fujit33 <thisis.snsd03@gmail.com>
2018-12-09 03:57:00 +0900

    mod ダジャレ判定の結果文字を修正

--- controllers/dajare_page.py
+++ controllers/dajare_page.py
@@ -27,6 +27,6 @@ class DajarePage:
         # ダジャレ判定
         dj = Dajarewake(dajare_text)
         is_dajare = dj.dajarewake()
-        wake_ans = "だじゃれじゃ" if is_dajare else "だじゃれじゃない"
+        wake_ans = "しゃれてんなぁ" if is_dajare else "なんて？"
 
         return render_template("dajaresult.html", dajare_text=dajare_text, wake_ans=wake_ans)
