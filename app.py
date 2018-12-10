83936c047fa6a898d083756c18d85c8f1448c21e
fujit33 <thisis.snsd03@gmail.com>
2018-12-11 01:43:25 +0900

    add 布団モードとブラウザモードを切り替え可能に変更

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
 from controllers import dajare_page
+import os
+import sys
+
+# 起動モード
+args = sys.argv
+mode = args[1] if (len(args) > 1 and args[1] == "futon") else "browser"
+print(mode + " モードで起動")
+
+# 環境変数に追加
+os.environ['MODE'] = mode
 
 # アプリケーションを作成
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
-
 
 # 分割先のコントローラー(Blueprint)を登録する
 app.register_blueprint(dajare_page.app)
