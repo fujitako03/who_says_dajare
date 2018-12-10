9018ce471f29e45cc89883976715347e9b8de917
fujit33 <thisis.snsd03@gmail.com>
2018-12-11 01:42:45 +0900

    fix サーボモータのクラスを変更

--- controllers/dajare_page.py
+++ controllers/dajare_page.py
@@ -1,5 +1,8 @@
 from flask import render_template, Blueprint, request
 from controllers.dajare_wake import Dajarewake
+from controllers.servo_moter import SG90
+import time
+import os
 
 # lueprintオブジェクトを生成します
 app = Blueprint('dajarepage', __name__)
@@ -27,6 +30,21 @@ class DajarePage:
         # ダジャレ判定
         dj = Dajarewake(dajare_text)
         is_dajare = dj.dajarewake()
-        wake_ans = "しゃれてんなぁ" if is_dajare else "なんて？"
+
+        # モーターを設定（futonモードの場合）
+        if os.getenv('MODE') == "futon":
+            servo = SG90(4, 50)
+
+        # 判定結果により
+        if is_dajare:
+            wake_ans = "ふっとんだ！"
+
+            # モーターを動かす（futonモードの場合）
+            if os.getenv('MODE') == "futon":
+                servo.setdirection(-50, 80)
+                time.sleep(0.1)
+                servo.cleanup()
+        else:
+            wake_ans = "なんて？"
 
         return render_template("dajaresult.html", dajare_text=dajare_text, wake_ans=wake_ans)
--- controllers/servo_moter.py
+++ controllers/servo_moter.py
@@ -1,26 +1,69 @@
 import RPi.GPIO as GPIO
+import time
+import atexit
 
 
-# SG92Rをコントロールするための
-class SG90_92R_Class:
-    # コンストラクタ
-    def __init__(self, pin, zero_offset_duty):
-        self.mPin = pin
-        self.m_ZeroOffsetDuty = zero_offset_duty
+class SG90:
+  """
+  サーボモータ（sg90）の操作のためのクラス
+  """
+  def __init__(self, pin, direction):
+    """
+    初期化する。
+    :param pin: GPIOのピン番号。PWMが使えること。
+    :param direction: 初期の向き。 -100(一番左) から 100(一番右)までの場所を整数値で指定する
+    :return: 
+    """
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(pin, GPIO.OUT)
+    self.pin = int(pin)
+    self.direction = int(direction)
+    self.servo = GPIO.PWM(self.pin, 50)
+    self.servo.start(0.0)
+    atexit.register(self.cleanup)
 
-        # GPIOをPWMモードにする
-        GPIO.setup(self.mPin, GPIO.OUT)
-        self.mPwm = GPIO.PWM(self.mPin, 50) # set 20ms / 50 Hz
+  def cleanup(self):
+    """
+    最後は正面に戻して終了する
+    :param self:
+    :return:
+    """
+    self.servo.ChangeDutyCycle(self.henkan(0))
+    time.sleep(0.3)
+    self.servo.stop()
+    GPIO.cleanup()
 
-    # 位置セット
-    def SetPos(self, pos):
-        # Duty ratio = 2.5%〜12.0% : 0.5ms〜2.4ms : 0 ～ 180deg
-        duty = (12-2.5)/180*pos+2.5 + self.m_ZeroOffsetDuty
-        self.mPwm.start(duty)
+  def currentdirection(self):
+    """
+    現在のSG90の向きを返す。
+    """
+    return self.direction
 
-    # 終了処理
-    def Cleanup(self):
-        # サーボを10degにセットしてから、インプットモードにしておく
-        self.SetPos(90)
-        time.sleep(1)
-        GPIO.setup(self.mPin, GPIO.IN)
+  def henkan(self, value):
+    """
+    ChangeDutyCycleに渡すための値を計算する。
+    -100から100のfloat値を入力して、2から12の値を返す。
+    ChangeDutyCycleに渡す値は 0.0 <= dc <= 100.0
+    ……のはずだが、なぜか2から12の間で動作している。
+    """
+    return 0.05 * value + 7.0
+
+  def setdirection(self, direction, speed):
+    """
+    SG90の向きを変える。
+    direction : -100 - 100 の整数値
+    speed     : 変化量
+    """
+    for d in range(self.direction, direction, int(speed)):
+      self.servo.ChangeDutyCycle(self.henkan(d))
+      self.direction = d
+      time.sleep(0.1)
+    self.servo.ChangeDutyCycle(self.henkan(direction))
+    self.direction = direction
+
+
+if __name__ == '__main__':
+    servo = SG90(4, 50)
+    servo.setdirection(-50, 80)
+    time.sleep(0.5)
+    servo.cleanup()
