676414e2bfd7fce75d0996957a611fe8b7b7d64c
woody-kawagoe <hip.hop.zip.please@gmail.com>
2019-04-14 22:57:16 +0900

    Docker wo docker ni yaru

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build --tag=who_says_dajare .
--- run.sh
+++
@@ -1,2 +0,0 @@
-# docker run -it --rm -p 8080:8080 -v $(pwd)/app:/home who_says_dajare:latest /bin/bash
-docker run -it --rm -p 5000:5000 -v $(pwd)/app:/home who_says_dajare:latest python3 home/main.py
