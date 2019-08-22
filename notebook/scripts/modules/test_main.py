b2d8172b11e7b8358b80e3a0bd10d75a9865cf91
菊池雄太郎 <yutaro_kikuchi@kikuchiyuutarounoMacBook-Pro.local>
2019-08-22 11:21:27 +0900

    Update script

--- notebook/scripts/modules/test_main.py
+++ notebook/scripts/modules/test_main.py
@@ -10,18 +10,18 @@ from datagenerator import Vocab, DataForGenerator
 
 train_data_path = os.path.join('../../data/test_data/', 'sentence_data.txt')
 id_train_data_path = os.path.join('../../data/test_data/', 'id_input.txt')
-input_label = os.path.join('../../data/test_data/sentence_label.txt')
+train_label_path = os.path.join('../../data/test_data/sentence_label.txt')
 model_path = './results/test_data/test.hdf5'
 
-vocab = Vocab(input_data)
+vocab = Vocab(train_data_path)
 
-vocab.write_word2id(input_data, id_input_data)
+vocab.write_word2id(train_data_path, id_train_data_path)
 
 batch_size = 30
 T = 25  # max_length of sentences
 emb_size = ''
 
-datagen = DataForGenerator(id_input_data, input_label, batch_size, T)
+datagen = DataForGenerator(id_train_data_path, train_label_path, batch_size, T)
 
 batch_size = 30
 T = 25
@@ -36,7 +36,7 @@ model = RNNModel(
     vocab_size=vocab_size,
     emb_size=emb_size,
     hidden_size=hidden_size,
-    T=T, 
+    T=T,
     dropout=dropout,
     lr=lr,
     model_path=model_path)
